3fa255c3706dff9fed1077392653107987ddbac1
Vishnu Hidesh <vishnuhideshnt@gmail.com>
2024-02-15 20:49:09 +0530

    Commit

---
+++ 7.py
@@ -0,0 +1,29 @@
+def merge_sort(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        left_half = arr[:mid]
+        right_half = arr[mid:]
+        merge_sort(left_half)
+        merge_sort(right_half)
+        i = j = k = 0
+        while i < len(left_half) and j < len(right_half):
+            if left_half[i] < right_half[j]:
+                arr[k] = left_half[i]
+                i += 1
+            else:
+                arr[k] = right_half[j]
+                j += 1
+            k += 1
+        while i < len(left_half):
+            arr[k] = left_half[i]
+            i += 1
+            k += 1
+        while j < len(right_half):
+            arr[k] = right_half[j]
+            j += 1
+            k += 1
+num_str = input("Enter a list of numbers separated by spaces: ")
+input_list = [int(num) for num in num_str.split()]
+print(f"Original list: {input_list}")
+merge_sort(input_list)
+print(f"Sorted list using Merge Sort: {input_list}")
